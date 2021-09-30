39f3789ecd61554d4b4cade74168dc48f71a6acd
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2021-09-30 20:59:49 +0530

    added comments

--- ANALYSER/analyser.java
+++ ANALYSER/analyser.java
@@ -1,18 +1,18 @@
 import java.io.FileReader;
 public class analyser {
     public static void analyse(String line){
-
+        //code to create symboltable
     }
     public static void main(String[] args)throws Exception{        
-        FileReader fr=new FileReader("./testcase/src.py");
-        String line=new String();
-        for(int i;(i=fr.read())!=-1;){
-            if((char)i=='\n'){
-                analyse(line);
-                line=new String();
+        FileReader fr=new FileReader("./testcase/src.py"); //get file
+        String line=new String(); //python is interpreted, so read line-by-line
+        for(int i;(i=fr.read())!=-1;){ //until end-of-file
+            if((char)i=='\n' || (char)i==';'){ //end-of-line
+                analyse(line); //pass to analysis phase
+                line=new String(); //make fresh line
             }
             else{
-                line+=((char)i);
+                line+=((char)i); //add charachters
             }
         }
         fr.close();
