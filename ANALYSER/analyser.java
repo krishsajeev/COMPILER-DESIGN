e1f43a4f9b5d8989c7e94be1be6375929785c3e3
Sanjaykumar-11 <sanjay484641@gmail.com>
2021-10-02 20:13:17 +0530

    Identification of tokens (keyword, sconst, id, nconst, op)

--- ANALYSER/analyser.java
+++ ANALYSER/analyser.java
@@ -1,10 +1,41 @@
 import java.io.FileReader;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.regex.*; 
+
 public class analyser {
+    static ArrayList<String> tokens = new ArrayList<String>(); //Arraylist to store tokens
+    
     public static void analyse(String line){
         //code to create symboltable
+        ArrayList<String> keywords = new ArrayList<String>(Arrays.asList("and","as","assert","break", "class", "continue", "def", "del", "del", "elif", "else", "except", "False", "finally", "for", "from", "global", "if", "import", "in", "lambda", "None", "nonlocal", "not", "or", "pass", "raise", "return", "True", "try", "while", "with", "yield"));
+        ArrayList<String> operators = new ArrayList<String>(Arrays.asList("+","-","*","/","%","**","//","=","+=","-=","*=","/=","%=","//=","**=","&=","|=","^=",">>=","<<=","==","!=",">","<",">=","<=", "and", "or", "not", "is", "is not", "in", "not in","&","|","^","~","<<",">>"));
+        for (String val: line.split("[' '.]+")) //split by whitespace, dot
+        {
+            if(keywords.contains(val)) //checks keyword
+            {
+                tokens.add("<keyword, "+ val +" >");
+            }
+            else if(Pattern.matches("(\")+(.)*(\")",val) || Pattern.matches("(\')+(.)*(\')",val)) //checks string constant
+            {
+                tokens.add("<sconst, "+ val +" >");
+            }
+            else if(Pattern.matches("([a-z]|[A-Z]|[_])+([0-9])*", val))//checks identifier
+            {
+                tokens.add("<id, "+ val +" >");
+            }
+            else if(Pattern.matches("[0-9]+", val))//checks number constant
+            {
+                tokens.add("<nconst, "+ val +" >");
+            }
+            else if(operators.contains(val))//checks operator
+            {
+                tokens.add("<op, "+ val +" >");
+            }
+        }
     }
     public static void main(String[] args)throws Exception{        
-        FileReader fr=new FileReader("./testcase/src.py"); //get file
+        FileReader fr=new FileReader("./testcase/simple_test.py"); //get file
         String line=new String(); //python is interpreted, so read line-by-line
         for(int i;(i=fr.read())!=-1;){ //until end-of-file
             if((char)i=='\n' || (char)i==';'){ //end-of-line
@@ -16,5 +47,7 @@ public class analyser {
             }
         }
         fr.close();
+        for (int i = 0; i < tokens.size(); i++) // loop for printing tokens
+            System.out.print(tokens.get(i) + "\n");
     }
 }
---
+++ ANALYSER/testcase/simple_test.py
@@ -0,0 +1,4 @@
+import math
+a = 10
+b = "ten"
+c = 20 * 50 / 5 
