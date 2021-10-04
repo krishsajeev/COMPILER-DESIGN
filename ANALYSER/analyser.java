2523223d5ffa01c2e1f3afcf7ee19a0d4ea974d5
Sanjaykumar-11 <sanjay484641@gmail.com>
2021-10-04 16:48:40 +0530

    Added Comment, delimiter tokens

--- ANALYSER/analyser.java
+++ ANALYSER/analyser.java
@@ -8,29 +8,41 @@ public class analyser {
     public static HashMap<String,String> analyse(String line){
         //code to create symboltable
         ArrayList<String> keywords = new ArrayList<String>(Arrays.asList("and","as","assert","break", "class", "continue", "def", "del", "del", "elif", "else", "except", "False", "finally", "for", "from", "global", "if", "import", "in", "lambda", "None", "nonlocal", "not", "or", "pass", "raise", "return", "True", "try", "while", "with", "yield"));
-        ArrayList<String> operators = new ArrayList<String>(Arrays.asList("+","-","*","/","%","**","//","=","+=","-=","*=","/=","%=","//=","**=","&=","|=","^=",">>=","<<=","==","!=",">","<",">=","<=", "and", "or", "not", "is", "is not", "in", "not in","&","|","^","~","<<",">>"));
+        ArrayList<String> operators = new ArrayList<String>(Arrays.asList("+","-","*","/","%","**","//",">","<",">=","<=", "and", "or", "not", "is", "is not", "in", "not in","&","|","^","~","<<",">>"));
+        ArrayList<String> delimiters = new ArrayList<String>(Arrays.asList("+=","-=","*=","/=","%=","//=","**=","&=","|=","^=",">>=","<<=","==","!=", "^=", ":", "`",";","[","]","(",")","{","}","=",",")); 
         HashMap<String,String> symboltable=new HashMap<String,String>();
-        for (String val: line.split("[' '.]+")) //split by whitespace, dot
+        if(line.charAt(0)=='#')
         {
-            if(keywords.contains(val)) //checks keyword
+            symboltable.put(line, "comment");
+        }
+        else
+        {
+            for (String val: line.split("[' '.]+")) //split by whitespace, dot
             {
-                symboltable.put(val,"keyword");
-            }
-            else if(Pattern.matches("(\")+(.)*(\")",val) || Pattern.matches("(\')+(.)*(\')",val)) //checks string constant
-            {
-                symboltable.put(val,"const");
-            }
-            else if(Pattern.matches("([a-z]|[A-Z]|[_])+([0-9])*", val))//checks identifier
-            {
-                symboltable.put(val,"id");
-            }
-            else if(Pattern.matches("[0-9]+", val))//checks number constant
-            {
-                symboltable.put(val,"const");
-            }
-            else if(operators.contains(val))//checks operator
-            {
-                symboltable.put(val,"op");
+                if(keywords.contains(val)) //checks keyword
+                {
+                    symboltable.put(val,"keyword");
+                }
+                else if(Pattern.matches("(\")+(.)*(\")",val) || Pattern.matches("(\')+(.)*(\')",val)) //checks string constant
+                {
+                    symboltable.put(val,"const");
+                }
+                else if(Pattern.matches("([a-z]|[A-Z]|[_])+([0-9])*", val))//checks identifier
+                {
+                    symboltable.put(val,"id");
+                }
+                else if(Pattern.matches("[0-9]+", val))//checks number constant
+                {
+                    symboltable.put(val,"const");
+                }
+                else if(operators.contains(val))//checks operator
+                {
+                    symboltable.put(val,"op");
+                }
+                else if(delimiters.contains(val)) //checks delimiter
+                {
+                    symboltable.put(val,"delimter");
+                }
             }
         }
         return symboltable;
